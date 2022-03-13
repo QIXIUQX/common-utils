4adffb94c48a30024f9038b9f0523b9d44591626
QIXIUQX <qingwu13950520@outlook.com>
2022-03-13 20:31:21 +0800

    增加vue项目的初始化配置文件

---
+++ 初始化vue项目的配置文件和样式文件/vue.config.js
@@ -0,0 +1,48 @@
+const {defineConfig} = require('@vue/cli-service')
+module.exports = defineConfig({
+	transpileDependencies: true,
+
+	//部署应用包时的基本 URL
+	publicPath: process.env.NODE_ENV === "production" ? "././" : "/",
+	configureWebpack: {
+		resolve: {
+			//配置别名
+			alias: {
+				assets: "@/assets",
+				components: "@/components",
+				network: "@/network",
+				views: "@/views",
+				utils: "@/utils",
+			},
+		},
+		devServer: {
+			host: "0.0.0.0",
+			port: 8888,
+			open: true,
+			https: false,
+			proxy: {
+				"/devApiServer": {
+					target: "http://localhost:3000",
+					changeOrigin: true,
+					ws: true,
+					pathRewrite: {
+						"^/devApiServer": "",
+					},
+				},
+			},
+		},
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				//additionalData: `@import "@/assets/scss/_var.scss";`, 最新版需要改为这个
+				additionalData: `
+				          @import "@/assets/scss/_var.scss";
+				          @import "@/assets/scss/mixin.scss";
+				          `,
+			},
+		},
+	},
+})
+
+
